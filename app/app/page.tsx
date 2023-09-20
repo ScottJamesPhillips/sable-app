2cc7228956ac3486accfd7b07ee3dfb6e9977de3
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2023-09-20 16:03:39 +0100

    Style for mac screen

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,7 +3,7 @@ import CompliantBonds from "@/components/app/compliant_bonds";
 
 function AppHome() {
   return (
-    <div className="flex flex-col h-screen grid grid-cols-7 grid-rows-5 gap-0 max-h-screen overflow-hidden text-white">
+    <div className="flex flex-col h-screen grid grid-cols-7 grid-rows-6 gap-0 max-h-screen overflow-hidden text-white">
       <div className="flex row-start-1 col-start-2 col-span-5 justify-between">
         <div>Available Bonds</div>
         <div className="flex">
@@ -11,13 +11,13 @@ function AppHome() {
           <div className="flex mx-5 text-gray-500 underline">Create Bond</div>
         </div>
       </div>
-      <div className="bg-sable-green-bg row-start-1 col-start-2 col-span-5 row-span-2 py-5 rounded-md my-5 flex items-stretch overflow-x-auto">
+      <div className="xl:h-96 bg-sable-green-bg row-start-1 col-start-2 col-span-5 row-span-3 py-5 rounded-md my-5 flex items-stretch overflow-x-auto">
         {/* <div>Available Bonds</div> */}
         <CompliantBonds />
       </div>
 
       {/* Table */}
-      <div className="flex col-start-2 row-start-3 col-span-5 row-span-3">
+      <div className="flex col-start-2 row-start-4 col-span-5 row-span-3 py-5 items-top">
         <BondsTable />
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -19,28 +19,28 @@ export default function Home() {
   return (
     //SP TODO - Maybe more grid rows and shit? Get stuff closer togeteh
     <div className="flex h-screen grid grid-cols-7 grid-rows-5 gap-0 max-h-screen overflow-hidden text-white">
-      <button onClick={test}>HELLO</button>
+      {/* <button onClick={test}>HELLO</button>
       <div>
         Current display status is: {isDisplayed == true ? "true" : "false"}
-      </div>
-      <div className="flex items-center h-1 col-start-3 row-start-3 col-span-4 text-8xl">
+      </div> */}
+      <div className="flex items-center h-1 col-start-3 row-start-2 col-span-4 text-8xl">
         <TbCurrencySolana
-          size={400}
+          size={350}
           className="text-sable-green-text stroke-[0.25px]"
           style={{ transform: "rotate(15deg)" }}
         />
         <div className="text-sable-green-text"> SABLE</div>
       </div>
-      <div className="flex text-sable-green-secondary-text text-6xl col-start-3 row-start-3 col-span-5 items-end">
+      <div className="flex text-sable-green-secondary-text text-6xl col-start-3 row-start-2 col-span-5 items-end">
         COMPLIANT BONDS
       </div>
-      <div className="flex relative col-start-1 row-start-6 col-span-2 row-span-2 ">
+      <div className="flex relative col-start-1 row-start-4 col-span-2 row-span-2 ">
         <FaAsterisk
-          size={400}
+          size={350}
           className="absolute bottom-0 right-0 text-sable-green-text"
         />
       </div>
-      <div className="bg-sable-green-bg row-start-4 col-start-3 col-span-5 row-span-2 p-5 rounded-md m-5 flex items-stretch overflow-x-auto">
+      <div className="xl:h-96 bg-sable-green-bg row-start-3 col-start-3 col-span-5 row-span-2 p-5 rounded-md m-5 flex items-stretch overflow-x-auto">
         <CompliantBonds />
       </div>
     </div>
