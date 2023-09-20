a7204a37e28a2f1b78cb44ddc74c1a76331780cd
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2023-09-20 16:03:18 +0100

    Remove AvailableBonds page route

--- app/available-bonds/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AvailableBond() {
-    return <h1>Available Bond</h1>
-  }
--- app/available-bonds/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AvailableBonds() {
-  return <h1>Available Bonds</h1>
-}
